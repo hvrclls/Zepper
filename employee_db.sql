f6d48dc44e0b8ab074db1fc1676c5271c068df52
Hever Calles <hevercalles@gmail.com>
2020-11-29 16:05:11 -0500

    setting up add employee

--- employee_db.sql
+++ employee_db.sql
@@ -9,18 +9,19 @@ CREATE TABLE department(
   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee_role(
-  id INT NOT NULL AUTO_INCREMENT,
-  role_title VARCHAR(30) NOT NULL,
-  salary DECIMAL (5,2),
-  dept_id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE employee_role(
+--   id INT NOT NULL AUTO_INCREMENT,
+  
+--   PRIMARY KEY (id)
+-- );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL AUTO_INCREMENT,
+  role_title VARCHAR(30) NOT NULL,
+  salary DECIMAL (5,2),
+  dept_id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id)
 );
--- tracker.js
+++ tracker.js
@@ -1,35 +1,79 @@
-var mysql = require("mysql")
+var mysql = require("mysql");
 var inquirer = require("inquirer");
-const { start } = require("repl");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "employee_db"
-})
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employee_db",
+});
 
-connection.connect(function(err){
-    if (err) throw err;
-    start()
-})
+connection.connect(function (err) {
+  if (err) throw err;
+  start();
+});
 
-function start(){
-    inquirer.prompt({
-        name: "actionList",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["View All Employees", "Add Employee", "Remove Employee"]
-    }).then(function(answer){
-        if (answer.actionList === "View All Employees"){
-            viewList()
-        }
-        else if (answer.actionList === "Add Employee"){
-            addEmployee()
-        }
-        else if (answer.actionList === "Remove Employee"){
-            addEmployee()
-        }
+function start() {
+  inquirer
+    .prompt({
+      name: "actionList",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "Add Employee", "Remove Employee"],
     })
+    .then(function (answer) {
+      if (answer.actionList === "View All Employees") {
+        viewList();
+      } else if (answer.actionList === "Add Employee") {
+        addEmployee();
+      } else if (answer.actionList === "Remove Employee") {
+        removeEmployee();
+      }
+    });
+}
+
+function viewList() {
+  inquirer.prompt([
+
+  ]);
+}
+
+function addEmployee() {
+  inquirer.prompt([
+    {
+        name: "firstName",
+        type: "input",
+        message: "Input Employee First Name: ",
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "Input Employee First Name: ",
+      },
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "Input Employee Department: ",
+      },
+      {
+        name: "newRole",
+        type: "input",
+        message: "Input Employee Role: ",
+      },
+      {
+        name: "newSalary",
+        type: "input",
+        message: "Input Employee Salary: ",
+      },
+  ]).then(function(answer){
+      "INSERT INTO employee SET ?",
+      {
+          
+      }
+  })
+}
+
+function removeEmployee() {
+  inquirer.prompt([]);
 }
