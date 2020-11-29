7f3fd90aa680d45958a789147e58dee7cbd75fca
Hever Calles <hevercalles@gmail.com>
2020-11-29 18:54:32 -0500

    employee info input working

--- tracker.js
+++ tracker.js
@@ -20,7 +20,7 @@ function start() {
       name: "actionList",
       type: "list",
       message: "What would you like to do?",
-      choices: ["View All Employees", "Add Employee", "Remove Employee"],
+      choices: ["View All Employees", "Add Employee", "Remove Employee", "Exit"],
     })
     .then(function (answer) {
       if (answer.actionList === "View All Employees") {
@@ -29,19 +29,47 @@ function start() {
         addEmployee();
       } else if (answer.actionList === "Remove Employee") {
         removeEmployee();
-      }
+      } else if (answer.actionList === "Exit") {
+        connection.end();
+      } 
     });
 }
 
 function viewList() {
-  inquirer.prompt([
-
-  ]);
+  inquirer.prompt([]);
 }
 
 function addEmployee() {
-  inquirer.prompt([
-    {
+  inquirer
+    .prompt({
+      name: "addAction",
+      type: "list",
+      message: "Input Information:",
+      choices: [
+        "Employee Information",
+        "Employee Role",
+        "Department Information",
+        "Return Home"
+      ],
+    })
+    .then(function (answer) {
+      if (answer.addAction === "Employee Information") {
+        employeeInfo();
+      } else if (answer.addAction === "Employee Role") {
+        employeeRole();
+      } else if (answer.addAction === "Department Information") {
+        deptInfo();
+      } else if (answer.addAction === "Return Home") {
+        start();
+      }
+      
+    });
+}
+
+function employeeInfo() {
+  inquirer
+    .prompt([
+      {
         name: "firstName",
         type: "input",
         message: "Input Employee First Name: ",
@@ -52,48 +80,89 @@ function addEmployee() {
         message: "Input Employee Last Name: ",
       },
       {
-        name: "newRole",
-        type: "input",
-        message: "Input Employee Role: ",
-      },
-      {
         name: "roleID",
         type: "input",
         message: "Input Role ID: ",
       },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.roleID,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Employee Information Added!");
+
+          addEmployee();
+        }
+      );
+    });
+}
+
+function employeeRole() {
+  inquirer
+    .prompt([
       {
-        name: "newDepartment",
+        name: "newRole",
         type: "input",
-        message: "Input Employee Department: ",
-      },
-      {
-        name: "departmentID",
-        type: "input",
-        message: "Input Department ID: ",
+        message: "Input Employee Role Title: ",
       },
       {
         name: "newSalary",
         type: "input",
         message: "Input Employee Salary: ",
       },
-  ])
-  .then(function(answer){
-
-    connection.query(
-      "INSERT INTO employees SET ?",
       {
-          first_name: answer.firstName,
-          last_name: answer.lastName,
-          role_id: answer.roleID || 0
+        name: "deptID",
+        type: "input",
+        message: "Input Employee Department ID: ",
       },
-      function (err) {
-          if (err) throw err
-          console.log("Employee Added!");
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employee_role SET ?",
+        {
+          role_title: answer.newRole,
+          salary: answer.newSalary,
+          dept_id: answer.deptID,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Employee Role Added!");
 
-          start()
-      }
-    )
-  })
+          addEmployee();
+        }
+      );
+    });
+}
+
+function deptInfo() {
+  inquirer
+    .prompt([
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "Input Employee Department: ",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          dept_name: answer.newDepartment,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Department Added!");
+
+          addEmployee();
+        }
+      );
+    });
 }
 
 function removeEmployee() {
